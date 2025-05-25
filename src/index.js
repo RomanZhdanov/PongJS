3721affbabb101999447f157147fcd8b297c74f3
Roman Zhdanov <zhdanov.roma@gmail.com>
2025-05-25 08:51:43 +0400

    Add package.json and parcel bundler.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Paddle from "/src/paddle";
+
+const canvas = document.getElementById("gameScreen");
+const ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+ctx.clearRect(0, 0, 800, 600);
+
+const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(ctx);
